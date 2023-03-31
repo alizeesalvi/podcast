5c5825e63ae86c494ad9c85890ce065e5d456b45
alizs <alizee.salvi@icloud.com>
2023-03-31 15:07:15 +0200

    CSS

--- resources/views/podcast.blade.php
+++ resources/views/podcast.blade.php
@@ -19,7 +19,7 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
         <a href="/">
-            <img src="logo.png" />
+            <img class="w-20" src={{asset("logo.png")}}>
         </a>
     </div>
 
